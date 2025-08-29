1862e006a2ebe1069a6c57851673f4d594359978
PabloRamos02 <p.ramos02@ufromail.cl>
2025-08-29 09:51:29 -0400

    Agregar archivos pom.xml y Main.java con estructura inicial del proyecto

---
+++ taller1/src/main/java/com/example/Main.java
@@ -0,0 +1,17 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+    public static void menu(){
+        System.out.println("Menu");
+        System.out.println("1. Verificar si una frase es un palíndromo");
+        System.out.println("2. Contar vocales en una frase");
+        System.out.println("3. encriptar una frase");
+        System.out.println("4. desencriptar una frase");
+        System.out.println("5. Salir");
+
+    }
+    
+}
